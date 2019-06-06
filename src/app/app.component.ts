aaf1fc9aa657ed4c2fc84070c2e1fa2fd8088946
dacardenasa <diego.cardenasaleg@gmail.com>
2019-06-05 22:56:11 -0500

    Update interface hangout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'ahorcado';
+
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+            'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  
+  constructor() {
+    console.log('Se acaba de crear la App Component');
+  }
+
 }
